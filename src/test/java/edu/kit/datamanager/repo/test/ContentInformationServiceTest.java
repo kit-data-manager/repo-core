71855227d9ee5f67c9b6043a743f8f0c4ad992d5
Thomas Jejkal <thomas.jejkal@kit.edu>
2023-01-11 19:00:52 +0100

    Fixed broken test ContentInformationServiceTest.findById

--- src/test/java/edu/kit/datamanager/repo/test/ContentInformationServiceTest.java
+++ src/test/java/edu/kit/datamanager/repo/test/ContentInformationServiceTest.java
@@ -132,7 +132,6 @@ public class ContentInformationServiceTest {
         ContentInformation found = service.findById(Long.toString(info.getId()));
         Assert.assertNotNull(found);
         Assert.assertEquals(info.getId(), found.getId());
-        System.out.println(found);
     }
 
     @Test(expected = ResourceNotFoundException.class)
